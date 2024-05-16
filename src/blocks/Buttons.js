e22a809330e940be4cd803aab705902f8b9902b3
heyyouhere <max.kalan@gmail.com>
2024-05-17 00:25:51 +0300

    Implemented buttons (Back and Home)

---
+++ src/blocks/Buttons.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+import { motion, useAnimate } from "framer-motion"
+
+
+export function BackButton() {
+   return (
+     <>
+        <div id='back_button_container' className="back_button_container">
+        </div>
+     </>
+   )
+}
+export function HomeButton() {
+   return (
+     <>
+        <div id='home_button_container' className="home_button_container">
+        </div>
+     </>
+   )
+}
