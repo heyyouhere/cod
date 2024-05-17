891a2a64da44441e163d079f34f254beecc586fc
Maxim Kalanchekaev <45402247+heyyouhere@users.noreply.github.com>
2024-05-17 17:42:40 +0300

    New gifs! (hope, last)

--- src/blocks/Partners.js
+++ src/blocks/Partners.js
@@ -3,35 +3,35 @@ import { useEffect } from "react"
 import { animate, delay, motion, useAnimate } from "framer-motion"
 import gif from "../images/icons_gif_loop_1.gif"
 
-import basis_logo     from "../images/logos/basis_logo.png"
-import kaspersky_logo from "../images/logos/kaspersky_logo.png"
-import skala_logo     from "../images/logos/skala_logo.png"
-import yadro_logo     from "../images/logos/yadro_logo.png"
+import basis_logo     from "../images/logos/basis_partner_button.png"
+import kaspersky_logo from "../images/logos/kaspersky_partner_button.png"
+import skala_logo     from "../images/logos/skala_partner_button.png"
+import yadro_logo     from "../images/logos/yadro_partner_button.png"
 
 
 const basis_data = {
     id: 'partner_basis',
     logo : basis_logo,
-    position : { x: 1100, y: 200 },
+    position : { x: 1000, y: 250 },
     text: "Вендор решений для организации динамической инфраструктуры, виртуальных рабочих мест и оказания облачных услуг"
 
 }
 const kaspersky_data = {
     id: 'partner_kaspersky',
     logo : kaspersky_logo,
-    position : { x: 1100, y: 625 },
+    position : { x: 1000, y: 625 },
     text: "Разработчик решений для защиты корпоративной ИТ-инфраструктуры"
 }
 const skala_data = {
     id: 'partner_skala',
     logo : skala_logo,
-    position : { x: 300, y: 200 },
+    position : { x: 250, y: 250 },
     text: "Производитель модульной платформы для построения корпоративной ИТ-инфраструктуры"
 }
 const yadro_data = {
     id: 'partner_yadro',
     logo : yadro_logo,
-    position : { x: 300, y: 625 },
+    position : { x: 250, y: 625 },
     text: "Разработчик решений для защиты корпоративной ИТ-инфраструктуры"
 }
 
@@ -40,11 +40,6 @@ function PartnerButton({partner_data}) {
     <motion.div initial={{ x: partner_data.position.x, y: partner_data.position.y}}>
         <div id={partner_data.id} className="partner_button">
             <img className="partner_button_logo"  src={partner_data.logo}></img>
-            <div className="partner_text">
-                {partner_data.text}
-            </div>
-            <div className="partner_button_detailes">
-            </div>
         </div>
     </motion.div>
  )
@@ -62,9 +57,8 @@ export default function PartnersMenu() {
 
     return (
         <>
-        <motion.div initial={{x: -400}}> 
-            <div id='about_perv_button' className='about_prev_button'></div>
-        </motion.div>
+        <div id='vendors_graphics' className="vendors_graphics"> </div>
+        <div id='vendors_text' className="vendors_text"> <b> ВЕНДОРЫ </b></div>
         <div id="partner_buttons_container_logos">
                 <PartnerButton partner_data={skala_data}></PartnerButton>
                 <PartnerButton partner_data={basis_data}></PartnerButton>
